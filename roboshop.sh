4dcfbf7588863992e28ea0adc00de5b6056e2a53
anil kumar <akthinq@gmailcom>
2025-05-21 11:50:40 +0530

    instance practice

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,7 @@ for instance in ${INSTANCES}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03884c9ac49287e7d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
-    if [ $instance -ne "frontend" ]
+    if [ $instance != "frontend" ]
     then
         IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
