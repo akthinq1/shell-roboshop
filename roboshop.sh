ce6c5704b3c1309d93a8646c831e4e6b03ff5edb
anil kumar <akthinq@gmailcom>
2025-05-21 11:53:43 +0530

    instance practice

--- roboshop.sh
+++ roboshop.sh
@@ -12,7 +12,9 @@ DOMAIN_NAME="akdevops.fun"
 
 #to create all instaince at one time
 
-for instance in ${INSTANCES[@]}
+# for instance in ${INSTANCES[@]}
+
+for insance in $@ #creates all instance
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03884c9ac49287e7d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
