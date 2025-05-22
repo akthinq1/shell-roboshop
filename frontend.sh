2736d0df654dcd74f8a8b225655f3e50acc77f21
anil kumar <akthinq@gmailcom>
2025-05-22 22:03:10 +0530

    frontend server

--- frontend.sh
+++ frontend.sh
@@ -58,7 +58,7 @@ VALIDATE $? "unzipping frontend"
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "Remove default nginx conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIRECTORY/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
