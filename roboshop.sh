ca1d004658e96c39bc1aeb0167251f8c6f5da241
anil kumar <akthinq@gmailcom>
2025-05-22 12:12:26 +0530

    instance practice

--- roboshop.sh
+++ roboshop.sh
@@ -24,7 +24,7 @@ DOMAIN_NAME="akdevops.fun"
 
 for instance in $@ 
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03884c9ac49287e7d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0f5f6a7a78c2680f3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]
     then
