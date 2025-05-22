1c77cd2a1eee033add8bb9ab22c0719a4682f5a6
anil kumar <akthinq@gmailcom>
2025-05-22 22:28:06 +0530

    frontend server

--- catalogue.sh
+++ catalogue.sh
@@ -79,8 +79,8 @@ VALIDATE $? "Copied mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE 
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.akdevops.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ $STATUS -lt 0 ]
+STATUS=$(mongosh --host 172.31.46.10 --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -le 0 ]
 then
     mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
