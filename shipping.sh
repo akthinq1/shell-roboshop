b196125a1db4262daf153d8ef0d20a3c0cd8d0bb
anil kumar <akthinq@gmailcom>
2025-05-24 13:00:54 +0530

    shipping rebbitmq payment

--- shipping.sh
+++ shipping.sh
@@ -80,15 +80,15 @@ dnf install mysql -y &>>$LOG_FILE
 #     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
 # fi
 
-DB_EXISTS=$(mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -sse "SHOW DATABASES LIKE 'cities';")
+DB_EXISTS=$(mysql -h mysql.akdevops.fun -u root -pRoboShop@1 -sse "SHOW DATABASES LIKE 'cities';")
 if [ "$DB_EXISTS" != "cities" ]; then
-    mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -e 'CREATE DATABASE cities;' &>> "$LOG_FILE"
+    mysql -h mysql.akdevops.fun -u root -pRoboShop@1 -e 'CREATE DATABASE cities;' &>> "$LOG_FILE"
     VALIDATE $? "Creating cities database"
-    mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/schema.sql &>> "$LOG_FILE"
+    mysql -h mysql.akdevops.fun -u root -pRoboShop@1 cities < /app/db/schema.sql &>> "$LOG_FILE"
     VALIDATE $? "Loading schema.sql"
-    mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/app-user.sql &>> "$LOG_FILE"
+    mysql -h mysql.akdevops.fun -u root -pRoboShop@1 cities < /app/db/app-user.sql &>> "$LOG_FILE"
     VALIDATE $? "Loading app-user.sql"
-    mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/master-data.sql &>> "$LOG_FILE"
+    mysql -h mysql.akdevops.fun -u root -pRoboShop@1 cities < /app/db/master-data.sql &>> "$LOG_FILE"
     VALIDATE $? "Loading master-data.sql"
 else
     echo -e "Database data is ${y}already loaded${reset}, skipping..." | tee -a "$LOG_FILE"
